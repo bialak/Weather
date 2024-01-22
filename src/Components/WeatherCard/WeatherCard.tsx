8d6062cdaca901b23c3adcce3615793c8ab286e1
Klaudia Biala <klaudiabiaa@gmail.com>
2024-01-22 12:09:53 +0100

    Create component witch fetched data

--- src/Components/WeatherCard/WeatherCard.tsx
+++ src/Components/WeatherCard/WeatherCard.tsx
@@ -1,21 +1,49 @@
+import { getCityWeather } from "Api/fetchDataCountry";
+import { useEffect, useState } from "react";
+import { QueryClient, QueryClientProvider, useQuery } from "@tanstack/react-query";
+import "./WeatherCard.css";
+
 type WeatherCardProps = {
-	country: string;
-	capital: string;
-	degrees: number;
-	wind: number;
-	image?: String;
+	// country: string;
+	city: string;
+	// degrees: number;
+	// wind: number;
+	// image?: String;
 };
 
-const WeatherSquere = ({ country, degrees, wind, image, capital }: WeatherCardProps) => {
+const WeatherCard = ({ city }: WeatherCardProps) => {
+	const { isPending, isError, data, error } = useQuery({
+		queryKey: ["weather", city],
+		queryFn: () => getCityWeather(city),
+	});
+
+	if (isPending) {
+		return <span>Loading...</span>;
+	}
+
+	if (isError) {
+		return <span>Error: {error.message}</span>;
+	}
+
+	console.log(data);
 	return (
-		<div>
-			<header>{country}</header>
-			<h2>{capital}</h2>
-			<h1>{degrees}</h1>
-			{image}
-			<h1>Wind {wind}</h1>
+		<div className="weatherCard">
+			<header className="country">{data.location.country}</header>
+			<h2 className="city"> {city}</h2>
+			<h2 className="text">{data.current.condition.text}</h2>
+			<h1 className="temperature"> {data.current.temp_c} °C</h1>
+			<h1 className="wind">Wind {data.current.wind_kph} kph</h1>
+			<img src={data.current.condition.icon} alt="" className="src" />
 		</div>
 	);
 };
 
-export default WeatherSquere;
+// <WeatherCard
+// 	country={countryWeather.country}
+// 	capital={countryWeather.name}
+// 	degrees={countryWeather.current.temp_c}
+// 	wind={countryWeather.current.wind_kph}
+// 	image={countryWeather.current.confition.icon}
+// />;
+
+export default WeatherCard;
