8f0b3eb7c8ad3424f074ca6f4af5c1e09d034f9c
Klaudia Biala <klaudiabiaa@gmail.com>
2024-01-26 14:04:25 +0100

    Create handle seraching cities

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,18 +1,45 @@
 import "./Search.css";
+import { fetchCitiesToSearcher } from "Api/fetchCitiesToSearcher";
+import { useCallback, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+
+var debounce = require("lodash.debounce");
 
 const Search = () => {
+	const [searchText, setSearchText] = useState("");
+
+	const { isLoading, data: searchResult } = useQuery({
+		queryKey: ["city-weather", searchText],
+		queryFn: () => fetchCitiesToSearcher(searchText),
+		enabled: Boolean(searchText),
+	});
+
 	function handleSubmit(e): void {
 		e.preventDefault();
-		console.log("this is:");
+	}
+
+	const debounceFn = useCallback(debounce(handleDebounceFn, 1000), []);
+
+	function handleDebounceFn(e) {
+		setSearchText(e.target.value);
+	}
+
+	function handleChange(e) {
+		debounceFn(e);
 	}
 
 	return (
 		<>
 			<h1 className="header">What the weather is like in your country </h1>
 			<form onSubmit={handleSubmit} className="searcher">
-				<input type="search" placeholder="Search..." />
-				<button type="submit">Search</button>
+				<input onChange={handleChange} type="search" placeholder="Search..." />
+				<button>Search</button>
 			</form>
+			<ul>
+				{searchResult?.map((result) => (
+					<li>{result.name}</li>
+				))}
+			</ul>
 		</>
 	);
 };
