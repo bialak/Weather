cd98691bcf6771b1bbbdab3a98cf794c4ee45ca9
Klaudia Biala <klaudiabiaa@gmail.com>
2024-01-26 14:01:27 +0100

    Move search component to QueryClientProvider

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import WeatherCard from "Components/WeatherCard/WeatherCard";
 import "./App.css";
 import Search from "./Components/Search/Search";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import WeatherCardsList from "Components/WeatherCardsList/WeatherCardsList";
 
 const queryClient = new QueryClient();
 
 function App() {
 	return (
 		<div className="App">
-			<Search />
 			<QueryClientProvider client={queryClient}>
-				<WeatherCard city={"London"} />;
+				<Search />
+				<WeatherCardsList />
 			</QueryClientProvider>
 		</div>
 	);
