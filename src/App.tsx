4c87fc8d42c7fc1dd587d23d348c2bc678699390
Klaudia Biala <klaudiabiaa@gmail.com>
2024-02-12 13:58:39 +0100

    Add Components and Hook to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import WeatherCard from "Components/WeatherCard/WeatherCard";
+import { useState } from "react";
 import "./App.css";
 import Search from "./Components/Search/Search";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import WeatherCardsList from "Components/WeatherCardsList/WeatherCardsList";
+import ResultSearchedCard from "Components/ResultSearchedCard/ResultSearchedCard";
 
 const queryClient = new QueryClient();
 
 function App() {
+	const [city, setCity] = useState("");
+
+	const handleSearchClick = (searchText: string) => {
+		setCity(searchText);
+	};
 	return (
 		<div className="App">
 			<QueryClientProvider client={queryClient}>
-				<Search />
-				<WeatherCardsList />
+				<Search onSearchClick={handleSearchClick} />
+				{city && <ResultSearchedCard city={city} />}
+				{!city && <WeatherCardsList />}
 			</QueryClientProvider>
 		</div>
 	);
