e2eee7d15315d8c1b8f6a4ded966fdc26668bf7d
Klaudia Biala <klaudiabiaa@gmail.com>
2024-02-12 13:56:17 +0100

    Create Search Component to find your weather city

--- src/Components/Search/Search.tsx
+++ src/Components/Search/Search.tsx
@@ -1,45 +1,85 @@
 import "./Search.css";
 import { fetchCitiesToSearcher } from "Api/fetchCitiesToSearcher";
-import { useCallback, useState } from "react";
+import { useCallback, useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 
-var debounce = require("lodash.debounce");
+const debounce = require("lodash.debounce");
 
-const Search = () => {
+type SearchProps = {
+	onSearchClick: (searchText: string) => void;
+};
+
+const Search = ({ onSearchClick }: SearchProps) => {
 	const [searchText, setSearchText] = useState("");
+	const [visible, setVisible] = useState(true);
 
-	const { isLoading, data: searchResult } = useQuery({
+	const { isLoading, data } = useQuery({
 		queryKey: ["city-weather", searchText],
 		queryFn: () => fetchCitiesToSearcher(searchText),
 		enabled: Boolean(searchText),
 	});
 
-	function handleSubmit(e): void {
-		e.preventDefault();
+	if (isLoading) {
+		<span className="loader"></span>;
 	}
 
-	const debounceFn = useCallback(debounce(handleDebounceFn, 1000), []);
+	const searchResult = data ?? [];
+
+	function handleSubmit(e): void {
+		e.preventDefault();
+		let listSearchedResults = searchResult.map((city) => city.name);
+		if (listSearchedResults.includes(searchText)) {
+			onSearchClick(searchText);
+			return;
+		} else {
+			alert("Write correct city");
+		}
+	}
+
+	const debounceFn = useCallback(debounce(handleDebounceFn, 500), []);
 
 	function handleDebounceFn(e) {
 		setSearchText(e.target.value);
+		setVisible(true);
 	}
 
 	function handleChange(e) {
+		setSearchText(e.target.value);
 		debounceFn(e);
 	}
 
+	function handleClick(clickedCity: string) {
+		setVisible(false);
+		setSearchText(clickedCity);
+	}
+
+	let tabIndexValue = 0;
 	return (
 		<>
 			<h1 className="header">What the weather is like in your country </h1>
 			<form onSubmit={handleSubmit} className="searcher">
-				<input onChange={handleChange} type="search" placeholder="Search..." />
-				<button>Search</button>
+				<input
+					className="searchInput"
+					onChange={handleChange}
+					type="search"
+					placeholder={"Search..."}
+					value={searchText}
+				/>
+				<button className="searchButton">Search</button>
 			</form>
-			<ul>
-				{searchResult?.map((result) => (
-					<li>{result.name}</li>
-				))}
-			</ul>
+			{visible && (
+				<ul className="searchPropositionList">
+					{searchResult?.map((result) => (
+						<li
+							className="searchProposition"
+							tabIndex={tabIndexValue++}
+							onClick={() => handleClick(result.name)}
+						>
+							{result.name}
+						</li>
+					))}
+				</ul>
+			)}
 		</>
 	);
 };
