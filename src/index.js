3ab029c5d306ddac8df204fbfbc5e5512421b26a
Klaudia Biala <klaudiabiaa@gmail.com>
2024-03-04 14:11:19 +0100

    delete redundant lines of code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-// import reportWebVitals from "./reportWebVitals";
 import React from "react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -10,5 +9,3 @@ root.render(
 		<App />
 	</React.StrictMode>
 );
-
-// reportWebVitals();
