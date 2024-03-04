5d7e64d75767295aab0f7b183778d34aa2352eac
Klaudia Biala <klaudiabiaa@gmail.com>
2024-03-04 14:15:33 +0100

    use functions from mockReqest

--- src/Components/WeatherCard/__test__/WeatherCards.test.jsx
+++ src/Components/WeatherCard/__test__/WeatherCards.test.jsx
@@ -1,29 +1,12 @@
 import { render, screen } from "test/setup";
 import WeatherCard from "../WeatherCard";
-import { getCityWeather } from "Api/getCityWeather";
+import { mockGetCityWeatherRequest } from "../../../test/mockRequests";
+
 const nock = require("nock");
 
 describe("WeatherCard", () => {
 	it("should show fetched data from function getWeatherCity", async () => {
-		const expectation = nock("http://api.weatherapi.com")
-			.defaultReplyHeaders({
-				"access-control-allow-origin": "*",
-			})
-			.get("/v1/current.json?key=c6068278a47d4030aad124640241101&q=Paris&aqi=no")
-			.reply(200, {
-				current: {
-					condition: {
-						text: "Sunny",
-						icon: "icon",
-					},
-					temp_c: 10,
-					wind_kph: 10,
-				},
-				location: {
-					country: "France",
-					name: "Paris",
-				},
-			});
+		mockGetCityWeatherRequest("Paris");
 
 		render(<WeatherCard city={"Paris"} />);
 		const countryInformation = await screen.findByTestId("country");
